ac4c9a1b2d42c59ebab0daaf07d7bf3494be84a0
Székely Szabolcs Ferenc <szekely.szabolcs@BOLYAI.LOCAL>
2026-01-16 08:42:28 +0100

    ALAP TARTALOM

---
+++ kitero/index.js
@@ -0,0 +1,69 @@
+/** 
+ * @typedef {{author: string, title1: string, concepts1: string, title2?: string,  concepts2?: string}} RowspanRowType  
+ * @typedef {{author: string, title: string, concepts: string, concepts2?: string}} ColspanRowType
+ * @typedef {{name: string, colSpan?: number}} HeaderType
+*/
+
+/** @type {HeaderType[]}  */
+const rowspanHeaderArr = [{name: "Szerző"}, {name: "Mű"}, {name: "Fogalmak"}] 
+/** @type {HeaderType[]}   */
+const colspanHeaderArr = [{name: "Szerző"},{name: "Mű"} , {name: "Fogalmak" ,colSpan: 2}] 
+
+/** @type {RowspanRowType[]}  */
+const rowspanBodyArr = [
+    {
+        author: "Appolliniare",
+        title1: "A megsebzett galamb és a szökőkút", 
+        concepts1: "képvers", 
+        title2: "Búcsú",
+        concepts2: "avantgárd" 
+    },
+    {
+        author: "Thomas Mann",
+        title1: "Mario és a varázsló",
+        concepts1: "kisregény" 
+    },
+    {
+        author: "Franz Kafka",
+        title1: "A per", 
+        concepts1: "képvers", 
+        title2: "Az átvlátozás", 
+        concepts2: "kisregény" 
+    }
+]
+
+
+
+/** @type {ColspanRowType[]} */
+const colspanBodyArr = [ 
+    {
+        author: "Appolliniare", 
+        title: "A megsebzett galamb és a szökőkút",
+        concepts: "Képvers",  
+        concepts2: "Emlékezés", 
+    },
+    {
+        author: "Appolliniare", 
+        title: "Búcsú", 
+        concepts: "Avantgárd" 
+    },
+    {
+        author: "Thomas Mann", 
+        title: "Mario és a varázsló", 
+        concepts: "Kisregény" 
+    },
+    {
+        author: "Franz Kafka",
+        title: "A per", 
+        concepts: "regény" 
+    },
+    {
+        author: "Franz Kafka", 
+        title: "Az átváltozás",
+        concepts: "kisregény", 
+        concepts2: "groteszk" 
+    }
+]
+
+renderColspanBody(makeTableBodyWithHeader(colspanHeaderArr), colspanBodyArr)
+renderRowspanBody(makeTableBodyWithHeader(rowspanHeaderArr), rowspanBodyArr)
